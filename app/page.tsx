b026209ae5b042b139dd0080b8e78aae25402d69
MadoGold <82536105+MadoGold@users.noreply.github.com>
2025-10-25 07:40:22 +0300

    Add main page, header, filters, pizzas

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,86 @@
-import { Button } from '@/components/ui/button';
+import {
+  Container,
+  TopBar,
+  Filters,
+  ProductCard,
+  ProductsGroupList,
+} from '@/components/shared';
+import { Title } from '@/components/shared/title';
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <>
+      <Container className="mt-10">
+        <Title text="Все пиццы" size="lg" className="font-extrabold" />
+      </Container>
+
+      <TopBar />
+
+      <Container className="mt-10 pb-14">
+        <div className="flex gap-[60px]">
+          <div className="w-[250px]">
+            <Filters />
+          </div>
+          <div className="flex-1">
+            <div className="flex flex-col gap-16">
+              <ProductsGroupList
+                title="Пиццы"
+                items={[
+                  {
+                    id: 1,
+                    name: 'Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:584x584/0198bf40eb1171aabe90b1b3ce07c0c5.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:584x584/0198bf40eb1171aabe90b1b3ce07c0c5.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:584x584/0198bf40eb1171aabe90b1b3ce07c0c5.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:584x584/0198bf40eb1171aabe90b1b3ce07c0c5.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:584x584/0198bf40eb1171aabe90b1b3ce07c0c5.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                  {
+                    id: 1,
+                    name: 'Сырная',
+                    imageUrl:
+                      'https://media.dodostatic.net/image/r:584x584/0198bf40eb1171aabe90b1b3ce07c0c5.avif',
+                    price: 550,
+                    items: [{ price: 550 }],
+                  },
+                ]}
+                categoryId={1}
+              />
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
 }
---
+++ components/shared/products-group-list.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { Title } from './title';
+import { cn } from '@/lib/utils';
+import { ProductCard } from './product-card';
+
+interface Props {
+  title: string;
+  items: any[];
+  categoryId: number;
+  listClassName?: string;
+  className?: string;
+}
+
+export const ProductsGroupList: React.FC<Props> = ({
+  title,
+  items,
+  categoryId,
+  listClassName,
+  className,
+}) => {
+  return (
+    <div className={className}>
+      <Title text={title} size="lg" className="font-extrabold mb-5" />
+
+      <div className={cn('grid grid-cols-3 gap-[50px]', listClassName)}>
+        {items.map((product) => (
+          <ProductCard
+            key={product.id}
+            id={product.key}
+            name={product.name}
+            imageUrl={product.imageUrl}
+            price={product.items[0].price}
+          />
+        ))}
+      </div>
+    </div>
+  );
+};
